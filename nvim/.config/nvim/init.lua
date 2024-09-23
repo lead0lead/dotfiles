8e6bf34e7e87f84838e055034cfcea78541b5ea8
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-09-23 11:42:31 +0200

    Replace nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,12 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
 
-require("vim-options")
-require("lazy").setup("plugins")
+
+require("config.lazy")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
